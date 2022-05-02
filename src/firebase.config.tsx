4deda68c9086456d9fbbe5d6ace9d990c5823d59
jamesv11 <jvanstrahlen@unicesar.edu.co>
2022-05-02 14:48:52 -0500

    hola

--- src/firebase.config.tsx
+++ src/firebase.config.tsx
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getStorage, ref, uploadBytes, getDownloadURL, getBytes } from "firebase/storage"
-import { getFirestore, collection, addDoc, getDocs, doc, getDoc, query, where, setDoc, deleteDoc  } from "firebase/firestore"
+import { getStorage,} from "firebase/storage"
+import { getFirestore} from "firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
